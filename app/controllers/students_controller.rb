89e1ac85ef9252f2f2a3472f9662738447c560ca
clifford <clifford@noukod.com>
2019-01-25 21:03:43 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,7 +17,7 @@ class StudentsController < ApplicationController
     @student.last_name = params[:last_name]
 
     @student.save
-    redirect_to students_path(@student)
+    redirect_to students_path
 
     # Student.create first_name: params[:first_name], last_name: params[:last_name]
     # redirect_to students_path
