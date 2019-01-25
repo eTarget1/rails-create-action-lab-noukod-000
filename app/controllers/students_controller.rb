46af6e4634377599e29f9035ef9592c791f3d584
clifford <clifford@noukod.com>
2019-01-25 20:38:22 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,10 +12,10 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post.title = params[:title]
-    @post.description = params[:description]
-    @post.save
+    @student = Student.new
+    @student.title = params[:title]
+    @student.description = params[:description]
+    @student.save
     redirect_to post_path(@post)
   end
 
